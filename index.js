5bbdee2f76e15d4808a8f812e799a8d8138d4feb
Mehedi Hasan Sarkar <mehedisarkar@hotmail.com>
2021-10-30 04:44:16 +0600

    database connection established

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
-// const { MongoClient } = require("mongodb");
+const { MongoClient } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -11,10 +11,29 @@ app.use(express.json());
 
 // connection URI
 // need uri link
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0ci0a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+// console.log(uri);
 
 // need to create a client
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 // create a server
+const server = async () => {
+  try {
+    await client.connect();
+    const database = client.db("travelVila");
+    const cruiseCollection = database.collection("cruiseCollection");
+
+    console.log("Database Connected");
+  } finally {
+    // await client.close();
+  }
+};
+
+server().catch(console.dir);
 
 app.get("/", (req, res) => {
   console.log("Travel Villa Server is running"); //this log will show on server terminal when client hit get request
